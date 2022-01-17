e3ac77f8983cce02153e9427f4d032b437c1547f
Andrew Wight <AndrewE_Wight@dfci.harvard.edu>
2022-01-17 11:04:54 -0500

    First commit to devel! Cleaned up preparePlot a fair bit, going to keep trying to simplify down this app so that I can be more confident in adding new features to it (like saving biex transforms, ideally).

--- R/preparePlot.R
+++ R/preparePlot.R
@@ -23,56 +23,90 @@
 preparePlot <- function(gs, sample, dims, subset, bins, coords, overlayGates,
                         addGateType, addCoords, useBiex, x_max, x_wide, x_pos,
                         x_neg, y_max, y_wide, y_pos, y_neg){
-    sample.gs <- gs[[sample]]
-    if(length(dims) > 2){
-        warning("gs_gate_interactive can only handle one or two dims.
-                The first two dims will be used, the others discarded.")
-    }
-    if(length(dims) == 1){
-        gg <- ggcyto::ggcyto(sample.gs, aes(!!dims[[1]]), subset = subset) +
-            geom_density() + scale_x_continuous(expand = c(0,0)) +
-            scale_y_continuous(expand = c(0,0)) + theme_flowGate
-        if(!is.null(coords)){
-            gg <- gg + coord_cartesian(xlim = coords[[1]])
-        }
-    } else {
-        gg <- ggcyto::ggcyto(
-            sample.gs, aes(!!dims[[1]], !!dims[[2]]), subset = subset) +
-                geom_hex(bins = bins) + scale_x_continuous(expand = c(0,0)) +
-                scale_y_continuous(expand = c(0,0)) + theme_flowGate
-        if(!is.null(coords)){
-            gg <- gg + coord_cartesian(xlim = coords[[1]], ylim = coords[[2]])
-        }
-    }
-    if(!is.null(overlayGates)){
-        gg <- gg + geom_gate(overlayGates)
-    } 
   
-    if(addGateType == "polygonGate"){
-        if(!is.null(addCoords) & nrow(addCoords) > 1){
-            gg <- gg + geom_path(
-                data = addCoords, aes(.data$x, .data$y), inherit.aes = FALSE)
-        }
-    }else if(addGateType == "quadGate"){
-        gg <- gg + ggplot2::geom_vline(xintercept = addCoords$X) +
-            ggplot2::geom_hline(yintercept = addCoords$Y)
-    }
-    if(useBiex){
-        suppressMessages(if(length(dims)==1){
-            gg <- gg + ggcyto::scale_x_flowjo_biexp(
-                maxValue = x_max, widthBasis = x_wide, pos = x_pos, neg = x_neg)
-        } else{
-            gg <- gg + ggcyto::scale_x_flowjo_biexp(
-                maxValue = x_max, widthBasis = x_wide, pos=x_pos, neg=x_neg) +
-                ggcyto::scale_y_flowjo_biexp(
-                maxValue = y_max, widthBasis = y_wide, pos = y_pos, neg = y_neg)
-        })
-    }
+    sample.gs <- gs[[sample]]
+    
+    gg <- prepMainPlot(sample.gs, dims, subset, bins, coords)
+    
+    if(!is.null(overlayGates)){gg <- gg + geom_gate(overlayGates)}
+    
+    gg <- drawGates(gg, addGateType, addCoords)
+  
+    gg <- biexAdjust(gg, useBiex, dims, x_max, x_wide, x_pos, x_neg, y_max, 
+                     y_wide, y_pos, y_neg)
+    
     gg <- ggcyto::as.ggplot(gg)
+    
     return(gg)
 }
 
+# Helper Functions -------------------------------------------------------------
 
-theme_flowGate <- theme_gray() + theme(
-        strip.background = element_blank(),
-        strip.text = element_blank())
+prepMainPlot <- function(sample.gs, dims, subset, bins, coords){
+  if(length(dims) > 2){
+    warning("gs_gate_interactive can only handle one or two dims.
+                The first two dims will be used, the others discarded.")
+  }
+  
+  if(length(dims) == 1){
+    gg <- ggcyto::ggcyto(sample.gs, aes(!!dims[[1]]), subset = subset) +
+      geom_density() + 
+      scale_x_continuous(expand = c(0,0)) +
+      scale_y_continuous(expand = c(0,0)) + 
+      theme_flowGate
+    if(!is.null(coords)){
+      gg <- gg + coord_cartesian(xlim = coords[[1]])
+    }
+  } else {
+    gg <- ggcyto::ggcyto(
+      sample.gs, aes(!!dims[[1]], !!dims[[2]]), subset = subset) +
+      geom_hex(bins = bins) + 
+      scale_x_continuous(expand = c(0,0)) +
+      scale_y_continuous(expand = c(0,0)) + 
+      theme_flowGate
+    if(!is.null(coords)){
+      gg <- gg + coord_cartesian(xlim = coords[[1]], ylim = coords[[2]])
+    }
+  }
+  
+  return(gg)
+}
+
+theme_flowGate <- theme_gray() + 
+  theme(
+    strip.background = element_blank(),
+    strip.text = element_blank()
+  )
+
+drawGates <- function(gg, addGateType, addCoords){
+  if(addGateType == "polygonGate"){
+    if(!is.null(addCoords) & nrow(addCoords) > 1){
+      gg <- gg + geom_path(
+        data = addCoords, aes(.data$x, .data$y), inherit.aes = FALSE)
+    }
+  }else if(addGateType == "quadGate"){
+    gg <- gg + ggplot2::geom_vline(xintercept = addCoords$X) +
+      ggplot2::geom_hline(yintercept = addCoords$Y)
+  }
+  
+  return(gg)
+}
+
+biexAdjust <- function(gg, useBiex, dims, x_max, x_wide, x_pos, x_neg, y_max, 
+                       y_wide, y_pos, y_neg){
+  if(useBiex){
+    suppressMessages(if(length(dims)==1){
+      gg <- gg + ggcyto::scale_x_flowjo_biexp(
+        maxValue = x_max, widthBasis = x_wide, pos = x_pos, neg = x_neg)
+    } else{
+      gg <- gg + ggcyto::scale_x_flowjo_biexp(
+        maxValue = x_max, widthBasis = x_wide, pos=x_pos, neg=x_neg) +
+        ggcyto::scale_y_flowjo_biexp(
+          maxValue = y_max, widthBasis = y_wide, pos = y_pos, neg = y_neg)
+    })
+  }
+  
+  return(gg)
+}
+
+
