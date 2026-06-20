d1c628da1edc4650a6f10ea72ac94c9f25eee307
David Rach <drach@som.umaryland.edu>
2026-06-20 15:59:31 -0400

    Added a GatingSet to openCyto template function, everything being written out as  a polygon (and registering the new method via openCyto). Will see whether works for other gate types.

---
+++ R/gs_to_openCyto.R
@@ -0,0 +1,92 @@
+
+#' Converts an existing GatingSet object into an openCyto template,
+#' based off gates existing for given sample. Experimental
+#' 
+#' @param gs A GatingSet object
+#' @param sample The sample index to use, default is 1
+#' @param outpath The file.path to the storage location. Default NULL will
+#' result in creation within the current working directory
+#' 
+#' @importFrom flowWorkspace gs_get_pop_paths
+#' @importFrom stringr str_equal
+#' @importFrom purrr map
+#' @importFrom dplyr bind_rows
+#' @importFrom utils write.csv
+#' 
+#' @return A .csv file containing an openCyto style template for
+#' subsequent re-gating attempts
+#' 
+#' @export
+#' 
+#' @examples A <- 2 + 2
+#' 
+gs_to_openCyto <- function(gs, sample=1, outpath=NULL, filename="openCytoTemplate"){
+  pops <- gs_get_pop_paths(gs)
+  pops <- pops[!str_equal("root", pops)]
+  CSV <- map(.x=pops, .f=gate_detallitos, gs=gs, sample=sample) |> bind_rows()
+
+  if (is.null(outpath)){outpath <- getwd()}
+
+  TheFilename <- paste0(filename, ".csv")
+  StorageLocation <- file.path(outpath, TheFilename)
+  write.csv(CSV, StorageLocation, row.names=FALSE)
+}
+
+#' Internal for gs_to_openCyto, retrieves details for individual gates
+#' 
+#' @param x The iterated in gate
+#' @param gs The GatingSet object
+#' @param sample The sample index being used for the openCyto template values
+#' 
+#' @importFrom flowWorkspace gs_pop_get_gate gs_pop_get_parent 
+#' 
+#' @return A data.frame row
+#' 
+#' @noRd
+#' 
+gate_detallitos <- function(x, gs, sample){
+  TheGate <- gs_pop_get_gate(gs[sample], x)[[1]]
+  TheVector <- TheGate@boundaries
+  coords <- paste(c(TheVector[,1], TheVector[,2]), collapse = ",")
+  TheMethod <- "polygon_gate"
+  TheDims <- colnames(TheVector)
+  if(length(TheDims) > 1){TheDims <- paste(TheDims, collapse = ",")}
+  ThefilterId <- TheGate@filterId
+  TheParent <- gs_pop_get_parent(gs[sample], x)
+  
+Data <- data.frame(
+  alias=ThefilterId,
+  pop="+",
+  parent=TheParent,
+  dims=TheDims,
+  gating_method=TheMethod,
+  gating_args= sprintf('matrix(c(%s),ncol=2)', coords),
+  collapseDataForGating="FALSE",
+  groupBy="NA",
+  preprocessing_method="NA",
+  preprocessing_args="NA"
+)
+  return(Data)
+}
+
+#' Defines polygon_gate for openCyto
+#' 
+#' @importFrom flowCore polygonGate
+#' 
+#' @noRd
+.my_polygon <- function(fr, pp_res, channels, boundaries, ...) {
+  colnames(boundaries) <- channels
+  flowCore::polygonGate(.gate = boundaries)
+}
+
+#' Ensures polygon_gate is available to openCyto
+#' 
+#' @importFrom openCyto register_plugins
+#' 
+#' @noRd
+.onLoad <- function(libname, pkgname) {
+  openCyto::register_plugins(
+    fun = .my_polygon,
+    methodName = "polygon_gate"
+  )
+}
